d143ea2c68e2e0b435e292c8057ec380e79e5295
Marcin Kasperski <Marcin.Kasperski@softax.pl>
2010-06-30 12:23:41 +0200

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '1.1.0'
+version = '1.1.1'
 long_description = open("README.txt").read()
 classifiers = [
     "Programming Language :: Python",
